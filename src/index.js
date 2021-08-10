cf107aac8ff75821fbdfab5e1cb20ee2e510828d
luist2411 <luiseduardotorresfigueroa@gmail.com>
2021-08-09 21:34:28 -0500

    u

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@
 // const jokUrl = 'https://api.chucknorris.io/jokes/random';
 
 // import { obtenerUser } from "./js/http-privider";
-import { init } from "./js/usuarios-page";
+// import { init } from "./js/usuarios-page";
 
-
+// import * as CRUD from "./js/CRUD";
+import { init  } from "./js/archivo-page";
 // fetch( jokUrl ).then( resp =>{
 // 	resp.json().then(({id, value}) =>{
 // 		console.log(id);
@@ -25,5 +26,26 @@ import { init } from "./js/usuarios-page";
 
 // init();
 // obtenerUser().then(console.log);
-init();
+// init();
 
+
+
+// CRUD.getUsuario(1).then(resp => {
+// 	console.log(resp);
+// });
+
+// CRUD.crearUsuario({
+
+// 	name: 'luis',
+// 	job: 'Web Developer'
+// }).then(console.log);
+
+// CRUD.actualizarUsuario( 1 , {
+// 	name: 'Ada',
+// 	job: 'Docente'
+// }).then(console.log)
+
+
+// CRUD.borrarUsuario(1).then(console.log);
+
+init();
--- src/js/http-privider.js
+++ src/js/http-privider.js
@@ -1,31 +1,55 @@
-const jokUrl  = 'https://api.chucknorris.io/jokes/random';
+const jokUrl = 'https://api.chucknorris.io/jokes/random';
 const urlUser = 'https://reqres.in/api/users?page=2';
 
-const obtenerChiste = async () =>{
+const cloudPreset = 't2eo5em6';
+const cloudUrl = ' https://api.cloudinary.com/v1_1/dpzixrda2/upload';
+
+
+const obtenerChiste = async () => {
 
 	try {
 		const respuesta = await fetch(jokUrl);
-	
+
 		if (!respuesta.ok) throw 'No se pudo realizar la peticion';
-	
-		const {icon_url, id , value} =  await respuesta.json();
-	
-		return  {icon_url, id , value} ;
-		
-	} catch (err ) {
+
+		const { icon_url, id, value } = await respuesta.json();
+
+		return { icon_url, id, value };
+
+	} catch (err) {
 		throw err
 	}
 }
 
-const obtenerUser = async ()=>{
+const obtenerUser = async () => {
 	const resp = await fetch(urlUser);
-	const {data: usuarios}  = await resp.json();
+	const { data: usuarios } = await resp.json();
 
 
 	return usuarios;
 }
 
+export const subirImagen = async (archivoSubir) => {
 
+	const formData = new FormData();
+	formData.append('upload_preset', cloudPreset);
+	formData.append('file', archivoSubir);
+	try {
+		const resp = await fetch(cloudUrl, {
+			method: 'POST',
+			body: formData
+		});
+		if (resp.ok) {
+			const cloudResp = await resp.json();
+			console.log(cloudResp);
+			return cloudResp.secure_url;
+		} else {
+			throw await resp.json();
+		}
+	} catch (err) {
+		throw err;
+	}
+}
 export {
 	obtenerChiste,
 	obtenerUser
